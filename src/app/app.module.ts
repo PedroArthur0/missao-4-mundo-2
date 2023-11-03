c8f0f3496e21d90506f110690c5d7c5273c2eb32
PedroArthur0 <136533459+PedroArthur0@users.noreply.github.com>
2023-11-03 17:55:31 -0300

    1 commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component';
+import { LivroDadosComponent } from './livro-dados/livro-dados.component';
+import { ControleLivrosService } from "./controle-livros.service";
+import { ControleEditoraService } from "./controle-editora.service";
+import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LivroListaComponent,
+    LivroDadosComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    ControleEditoraService,
+    ControleLivrosService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
